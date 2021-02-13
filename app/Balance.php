bf6de7cdf099a8b2aa667cf4bd7ebf10aed9ccf2
yurickvinicius <yurickvinicius@gmail.com>
2021-02-12 23:24:15 -0300

    Migrate and seeder of balance table finished

---
+++ app/Balance.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Balance extends Model
+{
+    protected $fillable = [
+        'balance',
+    ];
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(TypeUsersTableSeeder::class);
+        $this->call(BalancesTableSeeder::class);
     }
 }
